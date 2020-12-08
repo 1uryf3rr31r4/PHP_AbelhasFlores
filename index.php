331087b02fd827dc66025d251c954be4fce176ce
unknown <1uryf3rr31r4@gmail.com>
2020-12-08 14:32:14 -0300

    Entrada do projeto

---
+++ index.php
@@ -0,0 +1,60 @@
+<?php
+
+?>
+
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <title>Calendário de Flores</title>
+        <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+        <h1>Calendário de Flores</h1>
+        <button class="btn-cad">Cadastrar Flor</button>
+        <button class="btn-cad">Cadastrar Abelha</button>
+
+        <p>Neste calendário encontra-se diversas flores.<br>
+            Podem ser agrupadas pelos meses que florescem e<br>
+            o pelo tipo de abelha que poliniza a flor.
+        </p>
+
+        <p>Selecione as abelhas</p>
+        <select>
+            <option>1.  Uruçu (Melipona scutellaris);</option>
+            <option>2.  Uruçu-Amarela (Melipona rufiventris);</option>
+            <option>3.  Guarupu (Melipona bicolor);</option>
+            <option>4.  Iraí (Nannotrigona testaceicornes);</option>
+        </select>
+        
+        <p>Escolha os meses</p>
+        <div class="btn-grupo">
+            <button class="button">Jan</button>
+            <button class="button">Fev</button>
+            <button class="button">Mar</button>
+            <button class="button">Abr</button>
+            <button class="button">Mai</button>
+            <button class="button">Jun</button>
+            <button class="button">Jul</button>
+            <button class="button">Ago</button>
+            <button class="button">Set</button>
+            <button class="button">Out</button>
+            <button class="button">Nov</button>
+            <button class="button">Dez</button>
+        </div>
+        <div class="img-grupo">
+            <img src="" alt="">
+            <img src="" alt="">
+            <img src="" alt="">
+            <img src="" alt="">
+            <img src="" alt="">
+            <img src="" alt="">
+            <img src="" alt="">
+            <img src="" alt="">
+            <img src="" alt="">
+            <img src="" alt="">
+            <img src="" alt="">
+            <img src="" alt="">
+        </div>
+    </body>
+</html>
